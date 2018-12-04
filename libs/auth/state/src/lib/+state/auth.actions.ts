2f41df5ded38c987a4eb7b02780f20f18c98eb9a
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-12-04 17:00:19 +0200

    feat(auth): AuthActionTypes: LogoutConfirmation, LogoutDismiss

--- libs/auth/state/src/lib/+state/auth.actions.ts
+++ libs/auth/state/src/lib/+state/auth.actions.ts
@@ -4,6 +4,8 @@ import { User, AuthUserVW } from '../models/user';
 export enum AuthActionTypes {
   Login = '[Login Page] Login',
   Logout = '[User Page] Logout',
+  LogoutConfirmation = '[Auth API] Logout Confirmation',
+  LogoutDismiss = '[Auth API] Logout Dismiss',
   LoginSuccess = '[Auth API] Login Success',
   LoginFailure = '[Auth API] Login Failure',
   LoginRedirect = '[Auth API] Login Redirect'
@@ -35,17 +37,29 @@ export class Logout implements Action {
   readonly type = AuthActionTypes.Logout;
 }
 
+export class LogoutConfirmation implements Action {
+  readonly type = AuthActionTypes.LogoutConfirmation;
+}
+
+export class LogoutDismiss implements Action {
+  readonly type = AuthActionTypes.LogoutDismiss;
+}
+
 export type AuthAction =
   | Login
   | LoginSuccess
   | LoginFailure
   | LoginRedirect
-  | Logout;
+  | Logout
+  | LogoutConfirmation
+  | LogoutDismiss;
 
 export const fromAuthActions = {
   Login,
   LoginSuccess,
   LoginFailure,
   LoginRedirect,
-  Logout
+  Logout,
+  LogoutConfirmation,
+  LogoutDismiss
 };
