56af80cbb6cc1fa80d3560952f095fbbab6e4529
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-12-12 21:03:54 +0200

    feat(api): override 'morgan' and add it to 'express'

--- apps/api/src/app/app.ts
+++ apps/api/src/app/app.ts
@@ -1,6 +1,11 @@
 import * as express from 'express';
+import getLogger from './utils/logger';
 
 const app = express();
+
+const logger = getLogger(module);
+logger.debug(`Overriding 'Express' logger`);
+app.use( require('morgan')('combined', { 'stream': logger.stream }) );
 
 app.get('/', (req, res) => {
   res.send(`Welcome to api!`);
