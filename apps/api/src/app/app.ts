8fa0304f147fae276c7123c196fea06cf93abb2b
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-12-13 17:48:16 +0200

    feat(api): add 'cors()', 'cookieParser()', 'urlencoded()', 'json()'

--- apps/api/src/app/app.ts
+++ apps/api/src/app/app.ts
@@ -2,6 +2,11 @@ import * as express from 'express';
 import getLogger from './utils/logger';
 import { sendHttpError } from './middleware/sendHttpError';
 import { expressErrorHandler } from './middleware/expressErrorHandler';
+import { join } from 'path';
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const DIST_FOLDER = join(process.cwd(), 'dist');
 
 const app = express();
 
@@ -9,11 +14,22 @@ const logger = getLogger(module);
 logger.debug(`Overriding 'Express' logger`);
 app.use( require('morgan')('combined', { 'stream': logger.stream }) );
 
+app.use(cors());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use(sendHttpError);
+
+// ----------------------------------------------------------------------
 
 app.get('/', (req, res, next) => {
   res.json('Hello');
 });
+
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+
+// ----------------------------------------------------------------------
 
 app.use(expressErrorHandler);
 
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,11 @@
 import * as app from './app/app';
 import * as config from './app/config';
 import getLogger from './app/utils/logger';
-import { join } from 'path';
 
 const logger = getLogger(module);
 
 const PORT = config.get('port');
 const HOSTNAME =  process.env.NODE_ENV === 'production' ? config.get('hostname') : 'localhost';
-const DIST_FOLDER = join(process.cwd(), 'dist');
 
 app.listen(PORT, (err) => {
   if (err) { 
