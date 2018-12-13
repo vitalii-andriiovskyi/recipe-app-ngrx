c1a3cbe0a702ff97c4023f991cbef7bb3d5ae607
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-12-13 16:44:42 +0200

    refactor(api): add errors handling to app.ts

--- apps/api/src/app/app.ts
+++ apps/api/src/app/app.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import getLogger from './utils/logger';
+import { sendHttpError } from './middleware/sendHttpError';
+import { expressErrorHandler } from './middleware/expressErrorHandler';
 
 const app = express();
 
@@ -7,8 +9,18 @@ const logger = getLogger(module);
 logger.debug(`Overriding 'Express' logger`);
 app.use( require('morgan')('combined', { 'stream': logger.stream }) );
 
-app.get('/', (req, res) => {
-  res.send(`Welcome to api!`);
+app.use(sendHttpError);
+
+app.get('/', (req, res, next) => {
+  res.json('Hello');
 });
+
+app.use(expressErrorHandler);
+
+const errorhandler = require('errorhandler');
+if (process.env.NODE_ENV === 'development') {
+  // only use in development
+  app.use(errorhandler());
+}
 
 export = app;
