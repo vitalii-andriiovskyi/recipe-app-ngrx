d2a5f2cee21e22ccbbc65cc303cbcae35bbb0549
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-12-17 12:58:12 +0200

    refactor(api): move HttpError from index.ts to separate file

---
+++ apps/api/src/app/utils/error/http-error.ts
@@ -0,0 +1,19 @@
+const http = require('http');
+
+export const HTTP_ERROR_TYPE = 'customHttpError';
+
+export class HttpError extends Error {
+  status: number;
+  message: any;
+  // I added the prop 'type' in order to solve the issue with instanceof. There's workaround but it's also has problems
+  // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+  constructor(status: number, message?: string, public type = HTTP_ERROR_TYPE) {
+    // super(arguments);
+    super();
+    this.status = status;
+    this.message = message || http.STATUS_CODES[status] || 'Error';
+    
+    Error.captureStackTrace(this, HttpError);
+    // Error.captureStackTrace(this, this.constructor);
+  }
+}
