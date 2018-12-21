cf7f8b4d4d1ab419494deb27dcc7b4b2f55bbd08
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-12-21 13:45:18 +0200

    feat(utils): CommonErrorStateMatcher; reactive forms

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/auth.service';
 export * from './lib/local-storage.service';
 export * from './lib/log.service';
 export * from './lib/jwt-interceptor.service';
+export * from './lib/common-error-state-matcher';
