488c342d106e3f047a0e37fd5464e0f456a47997
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-04 19:01:29 +0200

    feat(recipe-state): recipeEntityMetadata

---
+++ libs/recipe/state/src/lib/recipe-entity-metadata.ts
@@ -0,0 +1,20 @@
+import { defaultSelectId, EntityMetadataMap, PropsFilterFnFactory } from 'ngrx-data';
+import { Recipe } from '@recipe-app-ngrx/models';
+
+export const recipeEntityMetadata: EntityMetadataMap = {
+  Recipe: {
+    sortComparer: sortByDateCreated,
+
+    // Pessimistic delete; optimistic add and update.
+    entityDispatcherOptions: {
+      optimisticDelete: false,
+      optimisticAdd: true,
+      optimisticUpdate: true
+    }
+  }
+};
+
+/** Sort Comparer to sort the entity collection by its name property */
+export function sortByDateCreated(a: Recipe, b: Recipe): number {
+  return +b.date_created - (+a.date_created);
+}
--- libs/recipe/state/src/lib/recipe-state.module.ts
+++ libs/recipe/state/src/lib/recipe-state.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ENTITY_METADATA_TOKEN } from 'ngrx-data';
+
+import { recipeEntityMetadata } from './recipe-entity-metadata';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  providers: [
+    { provide: ENTITY_METADATA_TOKEN, multi: true, useValue: recipeEntityMetadata }
+  ]
 })
 export class RecipeStateModule {}
