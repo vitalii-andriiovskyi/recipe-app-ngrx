838cd458bb4e137a1f76c9f9fba16aa6c79020a0
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-04 17:03:14 +0200

    refactor: AppModule: import RcpEntityStoreModule

--- apps/recipe-app/src/app/app.module.ts
+++ apps/recipe-app/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NxModule } from '@nrwl/nx';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
 import { storeFreeze } from 'ngrx-store-freeze';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { environment } from '../environments/environment';
+
 import { CustomRouterStateSerializer } from '@recipe-app-ngrx/utils';
 import { AuthStateModule } from '@recipe-app-ngrx/auth/state';
 import { AuthLoginUiModule } from '@recipe-app-ngrx/auth/login-ui';
-import { HttpClientModule } from '@angular/common/http';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedComponentsModule } from '@recipe-app-ngrx/shared-components';
+import { RcpEntityStoreModule } from '@recipe-app-ngrx/rcp-entity-store';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -33,7 +37,8 @@ import { SharedComponentsModule } from '@recipe-app-ngrx/shared-components';
     AuthStateModule,
     AuthLoginUiModule,
     FlexLayoutModule,
-    SharedComponentsModule
+    SharedComponentsModule,
+    RcpEntityStoreModule
   ],
   providers: [
     { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer }
