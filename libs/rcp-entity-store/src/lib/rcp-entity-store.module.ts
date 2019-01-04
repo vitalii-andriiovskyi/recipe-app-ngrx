e7d946142904da5ef57aa3cde402b3f8b0e8a311
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-04 16:58:40 +0200

    feat(rcp-entity-store): entityMetadata

--- libs/rcp-entity-store/src/lib/rcp-entity-store.module.ts
+++ libs/rcp-entity-store/src/lib/rcp-entity-store.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgrxDataModule, EntityServices } from 'ngrx-data';
+import { entityMetadata } from './entity-metadata';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    NgrxDataModule.forRoot({
+      entityMetadata: entityMetadata
+    })
+  ]
 })
 export class RcpEntityStoreModule {}
