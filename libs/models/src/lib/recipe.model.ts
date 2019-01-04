5ab8c15e8068e3461ed6ce89d8f4e3f5faff0698
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-04 11:09:40 +0200

    feat(model): Recipe

---
+++ libs/models/src/lib/recipe.model.ts
@@ -0,0 +1,20 @@
+import { Image } from './image.model';
+import { Ingredient } from './ingredient.model';
+
+export interface Recipe {
+  _id: number;
+  title: string;
+  description?: string;
+  ingredients: Ingredient[];
+  steps: string[];
+  images?: Image[];
+  footnotes?: string;
+  nutritionFat?: string;
+  preparetionTime?: number;
+  cookTime?: number;
+  servingsNumber?: number;
+
+  category: string;
+  user_username: string;
+  timestamp: string;
+}
