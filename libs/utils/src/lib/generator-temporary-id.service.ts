178dc6e5eeab7aee09366497df1521c0eb97a9d1
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-09 20:31:11 +0200

    refactor(utils): rename GeneratorTemporaryId into temporaryIdGenerator

--- libs/utils/src/lib/generator-temporary-id.service.ts
+++ libs/utils/src/lib/generator-temporary-id.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class GeneratorTemporaryId {
+export class TemporaryIdGenerator {
   private _id: number;
   lastBiggestBaseForId: number;
 
