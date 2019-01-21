f25cfe0791878d5798dfbbd17ab64ac3cd30018e
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-21 13:56:47 +0200

    feat(utils): function slugify()

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -4,4 +4,5 @@ export * from './lib/local-storage.service';
 export * from './lib/log.service';
 export * from './lib/jwt-interceptor.service';
 export * from './lib/common-error-state-matcher';
-export * from './lib/generator-temporary-id.service'
+export * from './lib/generator-temporary-id.service';
+export * from './lib/string-changer';
