7df3dfce12c2a1575a7b0f2b375d0d536bca48bc
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-21 16:51:46 +0200

    refactor(rcp-entity-store): AppEntityServices: RecipeEntityCollection...

--- libs/rcp-entity-store/src/lib/app-entity-services.ts
+++ libs/rcp-entity-store/src/lib/app-entity-services.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { EntityServicesElements, EntityServicesBase } from 'ngrx-data';
+import { RecipeEntityCollectionService } from '@recipe-app-ngrx/recipe/state';
 
 @Injectable()
 export class AppEntityServices extends EntityServicesBase {
   constructor(
     entityServicesElements: EntityServicesElements,
     // Inject custom services, register them with the EntityServices, and expose in API.
-    // public readonly villainsService: VillainsService
+    public readonly recipeEntityCollectionService: RecipeEntityCollectionService
   ) {
     super(entityServicesElements);
-    this.registerEntityCollectionServices([]);
+    this.registerEntityCollectionServices([recipeEntityCollectionService]);
   }
 
   // ... Additional convenience members
