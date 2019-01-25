f8531966f570e8cada3e99a8760971f8be6d4a79
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-25 18:33:27 +0200

    refactor(recipe-state): RcpEntityStoreModule, import RecipeStateModule

--- libs/rcp-entity-store/src/lib/rcp-entity-store.module.ts
+++ libs/rcp-entity-store/src/lib/rcp-entity-store.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { NgrxDataModule, EntityServices } from 'ngrx-data';
 import { entityMetadata } from './entity-metadata';
 import { AppEntityServices } from './app-entity-services';
+import { RecipeStateModule } from '@recipe-app-ngrx/recipe/state';
+
+export { AppEntityServices } from './app-entity-services';
 
 @NgModule({
   imports: [
     CommonModule,
     NgrxDataModule.forRoot({
       entityMetadata: entityMetadata
-    })
+    }),
+    RecipeStateModule
   ],
   providers: [
     AppEntityServices,
