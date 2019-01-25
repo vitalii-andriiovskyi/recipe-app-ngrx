b2a2ee0d9d4da37cd9dab87dd8ed9b20729de4be
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-25 17:54:21 +0200

    feat(recipe-state): recipeTotalNReducer

---
+++ libs/recipe/state/src/lib/+state/recipe.reducer.ts
@@ -0,0 +1,35 @@
+import { RecipeActionTypes, RecipeEntityOp } from './recipe.actions';
+import { EntityCollection, EntityAction } from 'ngrx-data';
+import { Recipe } from '@recipe-app-ngrx/models';
+
+const methods = {
+  [RecipeEntityOp.QUERY_TOTAL_N_RECIPES_SUCCESS]: (state: EntityCollection<Recipe>, action: EntityAction): EntityCollection<Recipe> => {
+    return {
+      ...state,
+      ...{ totalNRecipes: action.payload.data },
+    }
+  },
+  [RecipeEntityOp.QUERY_TOTAL_N_RECIPES_ERROR]: (state: EntityCollection<Recipe>, action: EntityAction): EntityCollection<Recipe> => {
+    return state;
+  },
+}
+
+export function recipeTotalNReducer(state: EntityCollection<Recipe>, action: EntityAction): EntityCollection<Recipe> {
+  state = methods[action.payload.entityOp] ? methods[action.payload.entityOp](state, action) : state;
+  // switch (action.payload.entityOp) {
+  //   case RecipeEntityOp.QUERY_TOTAL_N_RECIPES: {
+  //     state = {
+  //       ...state,
+  //       ...{ totalNRecipes: action.payload.data },
+  //     };
+  //     break;
+  //   }
+  //   // case RecipeActionTypes.TotalNRecipesLoadError: {
+  //   //   state = {
+  //   //     ...state
+  //   //   };
+  //   //   break;
+  //   // }
+  // }
+  return state;
+}
