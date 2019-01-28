c3481d6e25f6658bc6195eb30385c34c1e1446c0
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-28 19:57:56 +0200

    feat(modes): recipeCategoriesList

---
+++ libs/models/src/lib/recipe-categories-list.ts
@@ -0,0 +1,9 @@
+// Maybe this file shouldn't be in this this lib. But for this test project in could be
+export const recipeCategoriesList: string[] = [
+  'Breads & Rolls',
+  'Cookies & Candy',
+  'Soups, Stews & Chili',
+  'Salads',
+  'Desserts',
+  'Smoothies'
+];
