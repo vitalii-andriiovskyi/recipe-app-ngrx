3584304dffc8c7842e19c0e8014347a0b49d9e3f
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-01-29 12:55:04 +0200

    refactor(models): index.ts: add additional exports

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/user.model';
 export * from './lib/image.model';
 export * from './lib/ingredient.model';
 export * from './lib/recipe.model';
+export * from './lib/recipe-categories-list';
+export * from './lib/recipe-filters.model';
