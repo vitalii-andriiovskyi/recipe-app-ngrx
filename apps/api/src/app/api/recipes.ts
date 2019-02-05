aff3f4add15025f04705428a4a437d91964c8be4
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-05 13:22:59 +0200

    feat(api): RecipesApi: getRecipe(); refactor create(): add '/recipe/:id'

--- apps/api/src/app/api/recipes.ts
+++ apps/api/src/app/api/recipes.ts
@@ -1,12 +1,19 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { RecipeModel } from '../models/recipe';
+import { Recipe } from '@recipe-app-ngrx/models';
+import getLogger from '../utils/logger';
 
+const logger = getLogger(module);
 
 export class RecipesApi {
 
   public static create(router: Router): void {
     router.get('/recipes', (req: Request, res: Response, next: NextFunction) => {
       new RecipesApi().getRecipes(req, res, next);
+    });
+
+    router.get('/recipe/:id', (req: Request, res: Response, next: NextFunction) => {
+      new RecipesApi().getRecipe(req, res, next);
     })
   }
 
@@ -36,4 +43,29 @@ export class RecipesApi {
       })
       .catch(next);
   }
+
+  public getRecipe(req: Request, res: Response, next: NextFunction) {
+    // verify the id parameter exists
+    const PARAM_ID = 'id';
+    if (req.params[PARAM_ID] === undefined) {
+      res.status(404).send('Recipe not found.');
+      next();
+      return;
+    }
+
+    const id: number = parseInt(req.params[PARAM_ID], 10);
+
+    RecipeModel.findOne({ id: id }).then(recipe => {
+      if (recipe === null) {
+        res.status(404).send('Recipe not found.');
+        next();
+        return;
+      }
+
+      res.status(200).json(recipe.toObject());
+      logger.info('Recipe found and sent');
+      next();
+    }).catch(next);
+  }
+ 
 }
