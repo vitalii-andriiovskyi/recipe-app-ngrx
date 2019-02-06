8934838b7765be4058a670d0d644b53f6f7d6776
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-06 13:48:42 +0200

    feat(recipe-ui): RecipeEditorComponent

---
+++ libs/recipe/ui/src/lib/recipe-editor/recipe-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rcp-recipe-editor',
+  templateUrl: './recipe-editor.component.html',
+  styleUrls: ['./recipe-editor.component.scss']
+})
+export class RecipeEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/recipe/ui/src/lib/recipe-ui.module.ts
+++ libs/recipe/ui/src/lib/recipe-ui.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { RecipeEditorComponent } from './recipe-editor/recipe-editor.component';
 
 export const recipeUiRoutes: Route[] = [];
 
@@ -8,6 +9,7 @@ export const recipeUiRoutes: Route[] = [];
   imports: [
     CommonModule,
     RouterModule.forChild(recipeUiRoutes)
-  ]
+  ],
+  declarations: [RecipeEditorComponent]
 })
 export class RecipeUiModule {}
