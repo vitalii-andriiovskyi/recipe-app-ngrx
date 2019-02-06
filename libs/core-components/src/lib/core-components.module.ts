16f3f8186677373a75d0bbe5af6db42d795b0fd6
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-06 13:43:13 +0200

    refactor: move HeaderComp and PageNotFoundComp to CoreComponentsModule

--- libs/core-components/src/lib/core-components.module.ts
+++ libs/core-components/src/lib/core-components.module.ts
@@ -2,9 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 
-export const coreComponentsRoutes: Route[] = [];
+import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SharedComponentsModule } from '@recipe-app-ngrx/shared-components';
+
+export const coreComponentsRoutes: Route[] = [
+  { path: '**', component: PageNotFoundComponent }
+];
+
+const components = [
+  HeaderComponent,
+  PageNotFoundComponent
+]
 
 @NgModule({
-  imports: [CommonModule, RouterModule]
+  imports: [
+    SharedComponentsModule,
+    RouterModule.forChild(coreComponentsRoutes)
+  ],
+  declarations: [
+    components
+  ],
+  exports: [
+    components
+  ]
 })
 export class CoreComponentsModule {}
