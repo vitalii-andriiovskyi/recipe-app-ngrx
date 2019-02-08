e100e836e94f759e5fab10fbec01c0db9c3ae192
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-08 19:51:48 +0200

    feat(recipe-ui): RecipeEditorComponent: title, category, ingredients, steps, matcher

--- libs/recipe/ui/src/lib/recipe-ui.module.ts
+++ libs/recipe/ui/src/lib/recipe-ui.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { SharedComponentsModule } from '@recipe-app-ngrx/shared-components';
+
 import { RecipeEditorComponent } from './recipe-editor/recipe-editor.component';
 
-export const recipeUiRoutes: Route[] = [];
+export const recipeUiRoutes: Route[] = [
+  { path: 'create-recipe', component: RecipeEditorComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedComponentsModule,
     RouterModule.forChild(recipeUiRoutes)
   ],
-  declarations: [RecipeEditorComponent]
+  declarations: [RecipeEditorComponent],
+  exports: [RecipeEditorComponent]
 })
 export class RecipeUiModule {}
