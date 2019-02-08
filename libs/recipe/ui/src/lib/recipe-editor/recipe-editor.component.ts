6d1169f5b5a2ad185ac976d94709f7ad7bb8d9fe
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-08 19:49:08 +0200

    feat(recipe-ui): RecipeEditorComponent: recipeForm, createIngredient()

--- libs/recipe/ui/src/lib/recipe-editor/recipe-editor.component.ts
+++ libs/recipe/ui/src/lib/recipe-editor/recipe-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'rcp-recipe-editor',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-editor.component.scss']
 })
 export class RecipeEditorComponent implements OnInit {
+  recipeForm: FormGroup = new FormGroup({
+    id: new FormControl(0),
+    title: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    description: new FormControl(''),
+    ingredients: new FormArray([
+      this.createIngredient()
+    ], [Validators.required]),
+    steps: new FormArray([
+      new FormControl('')
+    ], [Validators.required]),
+    images: new FormArray([
+      // new FormGroup({
+      //   url: new FormControl(''),
+      //   altText: new FormControl(''),
+      //   titleText: new FormControl(''),
+      // })
+    ]),
+    footnotes: new FormControl(''),
+    nutritionFat: new FormControl(''),
+    preparationTime: new FormControl(''),
+    cookTime: new FormControl(''),
+    servingsNumber: new FormControl('')
+  });
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  createIngredient(): FormGroup {
+    return new FormGroup({
+      id: new FormControl(),
+      name: new FormControl(''),
+      unit: new FormControl(''),
+      quantity: new FormControl()
+    });
   }
 
 }
