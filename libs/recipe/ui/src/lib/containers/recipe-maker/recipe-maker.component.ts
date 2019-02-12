bca9f633526f29a7f5822e6aba1d889325e5e06f
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-12 18:01:49 +0200

    feat(recipe-ui): RecipeMakerComp: recipeById$, destroy$

--- libs/recipe/ui/src/lib/containers/recipe-maker/recipe-maker.component.ts
+++ libs/recipe/ui/src/lib/containers/recipe-maker/recipe-maker.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, combineLatest, Subject } from 'rxjs';
+import { tap, map, filter, switchMap, takeUntil, shareReplay } from 'rxjs/operators';
 
 import { RecipeEntityCollectionService } from '@recipe-app-ngrx/recipe/state';
 import { Recipe } from '@recipe-app-ngrx/models';
 import { AppEntityServices } from '@recipe-app-ngrx/rcp-entity-store';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rcp-recipe-maker',
   templateUrl: './recipe-maker.component.html',
   styleUrls: ['./recipe-maker.component.scss']
 })
-export class RecipeMakerComponent implements OnInit {
+export class RecipeMakerComponent implements OnInit, OnDestroy {
   recipeEntityService: RecipeEntityCollectionService;
 
+  recipeById$: Observable<Recipe>;
+  destroy$ = new Subject();
   constructor(
     private activatedRoute: ActivatedRoute, 
     private appEntityServices: AppEntityServices
@@ -22,6 +25,28 @@ export class RecipeMakerComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    this.recipeById$ = combineLatest(
+      this.activatedRoute.paramMap.pipe(map(paramMap => paramMap.get('id'))),
+      this.recipeEntityService.entityMap$
+    ).pipe(
+      filter(([id, entityMap]) => !!parseInt(id, 10)),
+      map(([id, entityMap]) => {
+        const recipe = entityMap[id];
+
+        if (!recipe) {
+          this.recipeEntityService.getByKey(id);
+        }
+        return recipe;
+      }),
+      takeUntil(this.destroy$),
+      shareReplay(1)
+    );
+    
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 
 }
