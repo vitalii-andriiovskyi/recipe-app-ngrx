d596d80fd2b783f0d43aaabe8836aac81faa958e
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-12 12:51:05 +0200

    feat(recipe-ui): RecipeMakerComponent

---
+++ libs/recipe/ui/src/lib/containers/recipe-maker/recipe-maker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rcp-recipe-maker',
+  templateUrl: './recipe-maker.component.html',
+  styleUrls: ['./recipe-maker.component.scss']
+})
+export class RecipeMakerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
