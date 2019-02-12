0095c856a171ec89ca3436e15d6e68b4c8e07b24
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-12 13:50:18 +0200

    feat(recipe-ui): RecipeMakerComp: recipeEntityService, appEntityServices

--- libs/recipe/ui/src/lib/containers/recipe-maker/recipe-maker.component.ts
+++ libs/recipe/ui/src/lib/containers/recipe-maker/recipe-maker.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { RecipeEntityCollectionService } from '@recipe-app-ngrx/recipe/state';
+import { Recipe } from '@recipe-app-ngrx/models';
+import { AppEntityServices } from '@recipe-app-ngrx/rcp-entity-store';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rcp-recipe-maker',
@@ -6,8 +12,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-maker.component.scss']
 })
 export class RecipeMakerComponent implements OnInit {
+  recipeEntityService: RecipeEntityCollectionService;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private appEntityServices: AppEntityServices
+  ) {
+    this.recipeEntityService = appEntityServices.recipeEntityCollectionService;
+  }
 
   ngOnInit() {
   }
