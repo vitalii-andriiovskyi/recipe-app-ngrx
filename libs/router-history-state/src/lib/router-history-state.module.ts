8f3a4e50ee52bd04d09feea4f98057217860100b
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-13 17:06:00 +0200

    feat(router-history-state): RouterHistoryFacade

--- libs/router-history-state/src/lib/router-history-state.module.ts
+++ libs/router-history-state/src/lib/router-history-state.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { ROUTERHISTORY_FEATURE_KEY, initialState as routerHistoryInitialState, routerHistoryReducer } from './+state/router-history.reducer';
+import { RouterHistoryEffects } from './+state/router-history.effects';
+import { RouterHistoryFacade } from './+state/router-history.facade';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule, 
+    StoreModule.forFeature(ROUTERHISTORY_FEATURE_KEY, routerHistoryReducer, { initialState: routerHistoryInitialState }),
+    EffectsModule.forFeature([RouterHistoryEffects])
+  ],
+  providers: [RouterHistoryFacade]
 })
 export class RouterHistoryStateModule {}
