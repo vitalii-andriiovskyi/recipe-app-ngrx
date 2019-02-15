643457ee957d9363896b4a5e538a4262e099ec47
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-15 20:27:50 +0200

    feat(recipe-ui): RecipeDetailComp: belongToCategory()

--- libs/recipe/ui/src/lib/components/recipe-detail/recipe-detail.component.ts
+++ libs/recipe/ui/src/lib/components/recipe-detail/recipe-detail.component.ts
@@ -18,4 +18,15 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit() {
   }
 
+  // This method is used in RecipeEntityCollectionService. Maybe I should extract it to separate file
+  belongToCategory(category: string, item: Recipe): boolean {
+    let result: boolean;
+    if (typeof item.category === 'string') {
+      result = item.category === category;
+    } else {
+      result = item.category.indexOf(category) > -1;
+    }
+    return result;
+  }
+
 }
