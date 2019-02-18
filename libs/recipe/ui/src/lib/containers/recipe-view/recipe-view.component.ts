4839ea46b2a28c1b61d1d9e7c0ee251875fba01b
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-18 18:45:30 +0200

    feat(recipe-ui): RecipeViewComp: _destroy$

--- libs/recipe/ui/src/lib/containers/recipe-view/recipe-view.component.ts
+++ libs/recipe/ui/src/lib/containers/recipe-view/recipe-view.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthFacade } from '@recipe-app-ngrx/auth/state';
+import { AppEntityServices } from '@recipe-app-ngrx/rcp-entity-store';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'rcp-recipe-view',
   templateUrl: './recipe-view.component.html',
   styleUrls: ['./recipe-view.component.scss']
 })
-export class RecipeViewComponent implements OnInit {
+export class RecipeViewComponent implements OnInit, OnDestroy {
+  private _destroy$ = new Subject()
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private authFacade: AuthFacade,
+    private appEntityServices: AppEntityServices
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this._destroy$.next();
   }
 
 }
