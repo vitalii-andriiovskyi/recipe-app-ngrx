fc6523c482292daa8a770fecaa415e3b5b9e87f4
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-18 18:35:36 +0200

    feat(recipe-ui): RecipeViewComponent

---
+++ libs/recipe/ui/src/lib/containers/recipe-view/recipe-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rcp-recipe-view',
+  templateUrl: './recipe-view.component.html',
+  styleUrls: ['./recipe-view.component.scss']
+})
+export class RecipeViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/recipe/ui/src/lib/recipe-ui.module.ts
+++ libs/recipe/ui/src/lib/recipe-ui.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Route } from '@angular/router';
 import { SharedComponentsModule } from '@recipe-app-ngrx/shared-components';
 
-import { RecipeEditorComponent } from './recipe-editor/recipe-editor.component';
+import { RecipeEditorComponent } from './components/recipe-editor/recipe-editor.component';
+import { RecipeMakerComponent } from './containers/recipe-maker/recipe-maker.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { AuthGuard } from '@recipe-app-ngrx/auth/state';
+import { RecipeViewComponent } from './containers/recipe-view/recipe-view.component';
+
+export { RecipeEditorComponent } from './components/recipe-editor/recipe-editor.component';
+export { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
 
 export const recipeUiRoutes: Route[] = [
-  { path: 'create-recipe', component: RecipeEditorComponent }
+  { path: 'create-recipe', component: RecipeMakerComponent, canActivate: [ AuthGuard ] },
+  { path: 'edit-recipe/:id', component: RecipeMakerComponent, canActivate: [ AuthGuard ] },
+  { path: 'recipe/:id', component: RecipeViewComponent },
 ];
 
 @NgModule({
@@ -13,7 +22,7 @@ export const recipeUiRoutes: Route[] = [
     SharedComponentsModule,
     RouterModule.forChild(recipeUiRoutes)
   ],
-  declarations: [RecipeEditorComponent],
-  exports: [RecipeEditorComponent]
+  declarations: [RecipeEditorComponent, RecipeMakerComponent, RecipeDetailComponent, RecipeViewComponent],
+  exports: [ RecipeEditorComponent, RecipeDetailComponent ]
 })
 export class RecipeUiModule {}
