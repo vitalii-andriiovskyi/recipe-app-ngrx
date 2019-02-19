d4991f1ac56b4f7c7d0ba2691b1f065272eee57e
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-19 18:09:21 +0200

    refactor(recipe-ui): RecipePreviewComponent: changeDetection

--- libs/recipe/ui/src/lib/components/recipe-preview/recipe-preview.component.ts
+++ libs/recipe/ui/src/lib/components/recipe-preview/recipe-preview.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Recipe } from '@recipe-app-ngrx/models';
 
 @Component({
   selector: 'rcp-recipe-preview',
   templateUrl: './recipe-preview.component.html',
-  styleUrls: ['./recipe-preview.component.scss']
+  styleUrls: ['./recipe-preview.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipePreviewComponent implements OnInit {
   @Input() recipe: Recipe;
