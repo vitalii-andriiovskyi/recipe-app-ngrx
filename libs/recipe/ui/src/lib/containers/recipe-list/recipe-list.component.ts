264c68189f51377d3ae131db0b1c4c03d66b23d3
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-19 18:50:40 +0200

    feat(recipe-ui): RecipeListComp: _destroy$, componentName

--- libs/recipe/ui/src/lib/containers/recipe-list/recipe-list.component.ts
+++ libs/recipe/ui/src/lib/containers/recipe-list/recipe-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppEntityServices } from '@recipe-app-ngrx/rcp-entity-store';
 import { RecipeEntityCollectionService } from '@recipe-app-ngrx/recipe/state';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Recipe } from '@recipe-app-ngrx/models';
 
 @Component({
@@ -9,12 +9,14 @@ import { Recipe } from '@recipe-app-ngrx/models';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
+  componentName = 'RecipeListComponent';
   recipeEntityService: RecipeEntityCollectionService;
 
   filteredRecipes$: Observable<Recipe[]>;
   countFilteredRecipes$: Observable<number>;
-  
+
+  private _destroy$ = new Subject();
   constructor(
     private appEntityServices: AppEntityServices,
 
@@ -25,6 +27,10 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
     this.filteredRecipes$ = this.recipeEntityService.filteredEntitiesByAllFilters$;
     this.countFilteredRecipes$ = this.recipeEntityService.countFilteredRecipes$;
+  }
+
+  ngOnDestroy() {
+    this._destroy$.next();
   }
 
 }
