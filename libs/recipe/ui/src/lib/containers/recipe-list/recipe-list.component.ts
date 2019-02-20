ca40edd9686cc57a94c440a0b905d45bd3f2d62e
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-20 13:44:47 +0200

    feat(recipe-ui): RecipeListComp: pageSize, inject Router, ActivatedRoute

--- libs/recipe/ui/src/lib/containers/recipe-list/recipe-list.component.ts
+++ libs/recipe/ui/src/lib/containers/recipe-list/recipe-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 import { AppEntityServices } from '@recipe-app-ngrx/rcp-entity-store';
 import { RecipeEntityCollectionService } from '@recipe-app-ngrx/recipe/state';
-import { Observable, Subject } from 'rxjs';
 import { Recipe } from '@recipe-app-ngrx/models';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'rcp-recipe-list',
@@ -12,6 +14,7 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
   componentName = 'RecipeListComponent';
+  pageSize = 3;
   recipeEntityService: RecipeEntityCollectionService;
 
   filteredRecipes$: Observable<Recipe[]>;
@@ -20,12 +23,14 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   private _destroy$ = new Subject();
   constructor(
     private appEntityServices: AppEntityServices,
-
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.recipeEntityService = appEntityServices.recipeEntityCollectionService;
   }
 
   ngOnInit() {
+    this.router.navigate(['./'], { relativeTo: this.route, queryParams: { page: 0, itemsPage: this.pageSize } })
     this.filteredRecipes$ = this.recipeEntityService.filteredEntitiesByAllFilters$;
     this.countFilteredRecipes$ = this.recipeEntityService.countFilteredRecipes$;
     this.loadRecipes();
