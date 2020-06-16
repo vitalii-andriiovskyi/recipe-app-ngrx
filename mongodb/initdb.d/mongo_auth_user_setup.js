a807adeff1463c67284e9059c73bb8f53d80a8fc
Vitalii Andriiovskyi <domovik0712@gmail.com>
2020-06-16 19:47:35 +0300

    feat: add test_rcp to mongoimage

--- mongodb/initdb.d/mongo_auth_user_setup.js
+++ mongodb/initdb.d/mongo_auth_user_setup.js
@@ -3,7 +3,8 @@ db.createUser(
     user: "vit",
     pwd: "secret",  // or cleartext password
     roles: [
-       { role: "readWrite", db: "rcp" }
+       { role: "readWrite", db: "rcp" },
+       { role: "readWrite", db: "test_rcp" },
     ]
   }
 )
