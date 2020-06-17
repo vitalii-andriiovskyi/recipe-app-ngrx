aaaaec3d9e63a227e265c0507d3b593e58a2127e
Vitalii Andriiovskyi <domovik0712@gmail.com>
2020-06-17 18:42:15 +0300

    feat: add test_rcp db to mongo

--- mongodb/initdb.d/mongo_auth_user_setup.js
+++ mongodb/initdb.d/mongo_auth_user_setup.js
@@ -1,10 +1,20 @@
+db = db.getSiblingDB('rcp')
 db.createUser(
   {
     user: "vit",
     pwd: "secret",  // or cleartext password
     roles: [
-       { role: "readWrite", db: "rcp" },
-       { role: "readWrite", db: "test_rcp" },
+       { role: "readWrite", db: "rcp" }
     ]
   }
 )
+
+db = db.getSiblingDB('test_rcp')
+
+db.createUser({
+  user: 'vit',
+  pwd: 'secret',
+  roles: [
+    { role: 'readWrite', db: 'test_rcp' }
+  ],
+});
