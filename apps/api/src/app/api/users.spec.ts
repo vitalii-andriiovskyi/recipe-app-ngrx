b2bd9a1b86ef883e2953f16cc099c4c3912271a2
Vitalii Andriiovskyi <domovik0712@gmail.com>
2020-06-22 20:31:28 +0300

    tests: userApi: session management

--- apps/api/src/app/api/users.spec.ts
+++ apps/api/src/app/api/users.spec.ts
@@ -51,14 +51,20 @@ describe('UserModel: MongoUserModel', () => {
     });
 
     it(`should get the user`, async () => {
-      const response = await request.get(`/api/users/${id}`);
+      let response = await request.post(`/api/users/authenticate`).send({username: userN.username, password: userN.password});
+      const { token } = response.body;
+
+      response = await request.get(`/api/users/${id}`).set('Authorization', `Bearer ${token}`);
       expect(response.body.username).toBe(userN.username);
       expect(response.body.hashedPassword).toBeFalsy();
     });
 
     it(`should return the error 403 when id is wrong`, async () => {
+      let response = await request.post(`/api/users/authenticate`).send({username: userN.username, password: userN.password});
+      const { token } = response.body;
+
       const wrongId = 'wrongId';
-      const response = await request.get(`/api/users/${wrongId}`);
+      response = await request.get(`/api/users/${wrongId}`).set('Authorization', `Bearer ${token}`);
       expect(response.status).toBe(403);
       expect(response['error']['text']).toContain(`User with ${wrongId} doesn't exist`);
     });
@@ -69,21 +75,25 @@ describe('UserModel: MongoUserModel', () => {
 
     it('should create the user', async() => {
       const response = await request.post('/api/users/create').send(userN);
+      const { userId, token, success } = response.body;
 
-      expect(response.body.username).toBe(userN.username);
-      await request.delete(`/api/users/${response.body._id}`).set('Authorization', `Bearer ${response.body.token}`);
+      expect(success).toBeTruthy();
+      expect(userId).toBeTruthy();
+      expect(token).toBeTruthy();
+      await request.delete(`/api/users/${userId}`).set('Authorization', `Bearer ${token}`);
 
     });
 
     it('should get error 403 when the user is created', async() => {
       const response = await request.post('/api/users/create').send(userN);
-      expect(response.body.username).toBe(userN.username);
+      const { userId, token, success } = response.body;
+      expect(success).toBeTruthy();
 
       const responseAgain = await request.post('/api/users/create').send(userN);
       expect(responseAgain.status).toEqual(403);
       expect(responseAgain['error']['text']).toBe(`Username ${userN.username} is already taken`);
 
-      await request.delete(`/api/users/${response.body._id}`).set('Authorization', `Bearer ${response.body.token}`);
+      await request.delete(`/api/users/${userId}`).set('Authorization', `Bearer ${token}`);
     });
 
     it(`shouldn't create the user when wrong body; Error 500`, async() => {
@@ -97,10 +107,19 @@ describe('UserModel: MongoUserModel', () => {
   // -------------------------------------------------------
 
   describe('DELETE /api/users/:id', () => {
-    let response: any;
+    let response: any,
+        token: string,
+        userId: string,
+        username: string;
 
     beforeEach(async () => {
       response = await request.post('/api/users/create').send(userN); 
+      token = response.body.token;
+      userId = response.body.userId;
+
+      const responseWithUser = await request.get(`/api/users/${userId}`).set('Authorization', `Bearer ${token}`);
+      const user = responseWithUser.body;
+      username = user.username;
     });
 
     afterEach(async () => {
@@ -111,82 +130,145 @@ describe('UserModel: MongoUserModel', () => {
     });
 
     it(`should remove user`, async() => {
-      expect(response.body.username).toBe(userN.username);
+      expect(username).toBe(userN.username);
 
-      const deletionRes = await request.delete(`/api/users/${response.body._id}`).set('Authorization', `Bearer ${response.body.token}`);
+      const deletionRes = await request.delete(`/api/users/${userId}`).set('Authorization', `Bearer ${token}`);
       expect(deletionRes.status).toEqual(200);
     });
 
     it(`should response with err 403 if user got removed earlier`, async() => {
-      expect(response.body.username).toBe(userN.username);
+      expect(username).toBe(userN.username);
 
-      let deletionRes = await request.delete(`/api/users/${response.body._id}`).set('Authorization', `Bearer ${response.body.token}`);
+      let deletionRes = await request.delete(`/api/users/${userId}`).set('Authorization', `Bearer ${token}`);
       expect(deletionRes.status).toEqual(200);
 
-      deletionRes = await request.delete(`/api/users/${response.body._id}`).set('Authorization', `Bearer ${response.body.token}`);
+      deletionRes = await request.delete(`/api/users/${userId}`).set('Authorization', `Bearer ${token}`);
       expect(deletionRes.status).toEqual(403);
       // console.log(deletionRes.error);
-      expect(deletionRes['error']['text']).toBe(`There's no user with id: ${response.body._id}`);
+      expect(deletionRes['error']['text']).toBe(`There's no user with id: ${userId}`);
     });
 
     it(`should response with err 403 if user doesn't exist`, async() => {
-      expect(response.body.username).toBe(userN.username);
-      const id = response.body._id;
-      let newId = createNewId(id, 'a');
-      newId = id !== newId ? newId : createNewId(id, 'b');
+      expect(username).toBe(userN.username);
+      let newId = createNewId(userId, 'a');
+      newId = userId !== newId ? newId : createNewId(userId, 'b');
   
-      const deletionRes = await request.delete(`/api/users/${newId}`).set('Authorization', `Bearer ${response.body.token}`);
+      const deletionRes = await request.delete(`/api/users/${newId}`).set('Authorization', `Bearer ${token}`);
       expect(deletionRes.status).toEqual(403);
       expect(deletionRes['error']['text']).toBe(`There's no user with id: ${newId}`);
     });
 
     it(`should response with err 500 if id isn't objectID`, async() => {
-      expect(response.body.username).toBe(userN.username);
-      const newId = response.body._id + 'a';
+      expect(username).toBe(userN.username);
+      const newId = userId + 'a';
   
-      const deletionRes = await request.delete(`/api/users/${newId}`).set('Authorization', `Bearer ${response.body.token}`);
+      const deletionRes = await request.delete(`/api/users/${newId}`).set('Authorization', `Bearer ${token}`);
       expect(deletionRes.status).toEqual(500);
     });
   });
 
   describe('POST /api/users/authenticate', () => {
-
+    let user,
+        id: string;
     beforeAll(async () => {
       await UserModel.create(userN as any); 
+      user = await UserModel.findOne({username: userN.username});
+      id = `` + user._id;
+
+      // redisClient.on('error', (err) => {
+      //   console.log(err);
+      // })
+      // redisClient.on('connect', () => {
+      //   done();
+      // })
     });
 
     afterAll(async () => {
-      const user = await UserModel.findOne({username: userN.username});
+      user = await UserModel.findOne({username: userN.username});
       if (user && user.username === userN.username) {
         await user.remove();
       }
     });
 
-    it(`should authenticate the user`, async () => {
-      const response = await request.post(`/api/users/authenticate`).send({username: userN.username, password: userN.password});
-      expect(response.body.username).toBe(userN.username);
-      expect(response.body.token).toBeTruthy();
-      expect(response.body.hashedPassword).toBeFalsy();
+    describe(`There's no token in request`, () => {
+      it(`should authenticate the user`, async () => {
+        const response = await request.post(`/api/users/authenticate`).send({username: userN.username, password: userN.password});
+        const { userId, success, token } = response.body;
+        expect(userId).toBe(id);
+        expect(token).toBeTruthy();
+        expect(success).toBeTruthy();
+      });
+
+      it(`should return the error 403 when the password is wrong`, async () => {
+        const response = await request.post(`/api/users/authenticate`).send({username: userN.username, password: '2222'});
+        expect(response.status).toBe(401);
+        expect(response['error']['text']).toBe(`Password is incorrect`);
+      });
+
+      it(`should return the error 403 when the user doesn't exist`, async () => {
+        const response = await request.post(`/api/users/authenticate`).send({username: 'nobody', password: userN.password});
+        expect(response.status).toBe(401);
+        expect(response['error']['text']).toBe(`User nobody doesn't exist`);
+
+      });
+
+      it(`should return the error 500 when only the username is sent`, async () => {
+        const response = await request.post(`/api/users/authenticate`).send({username: userN.username});
+        expect(response.status).toBe(500);
+      });
+
+    
+    });
+    describe(`There's token in request`, () => {
+      it(`should return the id of a user`, async () => {
+        expect.assertions(4);
+        let response = await request.post(`/api/users/authenticate`).send({username: userN.username, password: userN.password});
+        const { success, token } = response.body;
+        let { userId } = response.body;
+        expect(userId).toBe(id);
+        expect(token).toBeTruthy();
+        expect(success).toBeTruthy();
+
+        response = await request
+          .post(`/api/users/authenticate`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({username: userN.username, password: userN.password});
+
+        userId = response.body.userId;
+        expect(userId).toBe(id);
+
+      });
+      it(`shouldn't return the id of a user, when token is wrong`, async () => {
+        expect.assertions(5);
+        let response = await request.post(`/api/users/authenticate`).send({username: userN.username, password: userN.password});
+        const { success, userId } = response.body;
+        let { token } = response.body;
+        expect(userId).toBe(id);
+        expect(token).toBeTruthy();
+        expect(success).toBeTruthy();
+
+        token = token.slice(0, -1) + 'A';
+
+        response = await request
+          .post(`/api/users/authenticate`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({username: userN.username, password: userN.password});
+
+        const message = response.text;
+        const status = response.status;
+        expect(message).toBe('Unauthorized');
+        expect(status).toBe(400);
+
+      });
     });
 
-    it(`should return the error 403 when the password is wrong`, async () => {
-      const response = await request.post(`/api/users/authenticate`).send({username: userN.username, password: '2222'});
-      expect(response.status).toBe(401);
-      expect(response['error']['text']).toBe(`Password is incorrect`);
-    });
-
-    it(`should return the error 403 when the user doesn't exist`, async () => {
-      const response = await request.post(`/api/users/authenticate`).send({username: 'nobody', password: userN.password});
-      expect(response.status).toBe(401);
-      expect(response['error']['text']).toBe(`User nobody doesn't exist`);
-
-    });
-
-    it(`should return the error 500 when only the username is sent`, async () => {
-      const response = await request.post(`/api/users/authenticate`).send({username: userN.username});
-      expect(response.status).toBe(500);
-    });
-
+    // has token -> give id
+    // hasn't token -> check username and pass and allow authentication
+    //              -> if allowed store token in redis 
+    //                    -> if token stored successfull return obj SuccessAuth
+    //                    -> if token didn't store, return error message.
+    //              -> if didn't allow, return error AuthError.
+    
   });
 
 });
