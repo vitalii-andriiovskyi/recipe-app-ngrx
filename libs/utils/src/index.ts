e2737e6a34b9e5b9ae314d1b6e2f73f2c7292664
Vitalii Andriiovskyi <domovik0712@gmail.com>
2020-06-25 17:26:38 +0300

    feat: SessionStorageService

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/routerStateSerializer';
 export * from './lib/local-storage.service';
+export * from './lib/session-storage.service';
 export * from './lib/log.service';
 export * from './lib/jwt-interceptor.service';
 export * from './lib/common-error-state-matcher';
