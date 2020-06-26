67b4521d7bf1eca57945477b4724d8337f8303c1
Vitalii Andriiovskyi <domovik0712@gmail.com>
2020-06-26 18:00:38 +0300

    feat: UserEffects: loadUserSuccess$

--- libs/user/state/src/lib/+state/user.effects.ts
+++ libs/user/state/src/lib/+state/user.effects.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { switchMap, map, catchError } from 'rxjs/operators';
+import { switchMap, map, catchError, tap, withLatestFrom } from 'rxjs/operators';
 
 import * as fromUser from './user.reducer';
 import * as UserActions from './user.actions';
 import { UserService } from '../services';
 import { SessionStorageService } from '@recipe-app-ngrx/utils';
+import { AuthFacade } from '@recipe-app-ngrx/auth/state';
 import { of } from 'rxjs';
+import { SessionData } from '@recipe-app-ngrx/models';
 
 @Injectable()
 export class UserEffects {
@@ -24,9 +26,22 @@ export class UserEffects {
     )
   );
 
+  loadUserSuccess$ = createEffect(() => 
+    this.actions$.pipe(
+      ofType(UserActions.loadUserSuccess),
+      withLatestFrom(this.authFacadeService.loggedIn$),
+      tap(([action, loggedIn]) => {
+        const session: SessionData = this.sessionStorageService.getItem('currentUser');
+        if (session && !loggedIn) {
+          this.authFacadeService.loginSuccess(session);
+        }
+      })
+    ), { dispatch: false })
+
   constructor(
     private actions$: Actions,
     private userService: UserService,
-    private sessionStorageService: SessionStorageService
+    private sessionStorageService: SessionStorageService,
+    private authFacadeService: AuthFacade
   ) {}
 }
